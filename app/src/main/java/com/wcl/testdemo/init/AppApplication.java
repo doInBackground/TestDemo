2a1dd746a987cef5af9006262ff464cc4e637113
chunle.wei <chunle.wei@timanetworks.com>
2023-08-15 18:39:57 +0800

    修改: 优化日志系统,支持动态配置日志输出和落地的过滤器等级.

--- app/src/main/java/com/wcl/testdemo/init/AppApplication.java
+++ app/src/main/java/com/wcl/testdemo/init/AppApplication.java
@@ -9,10 +9,17 @@ import com.blankj.utilcode.util.ActivityUtils;
 import com.blankj.utilcode.util.AppUtils;
 import com.blankj.utilcode.util.CrashUtils;
 import com.blankj.utilcode.util.DeviceUtils;
+import com.blankj.utilcode.util.FileIOUtils;
 import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.PathUtils;
 import com.blankj.utilcode.util.ProcessUtils;
 import com.blankj.utilcode.util.ScreenUtils;
 import com.blankj.utilcode.util.Utils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
 
 import androidx.annotation.NonNull;
 
@@ -38,24 +45,40 @@ public class AppApplication extends Application {
 
     //初始化日志.
     private void initLog() {
+        boolean isDebugLog = AppUtils.isAppDebug();//是否调试日志.
+        //读取配置文件.
+//        String configRootPath = PathUtils.getInternalAppDataPath();//APP内部沙箱根路径(配置文件的更改方式,如果是"按钮程序"修改,则配置文件放在内部沙箱(安全)).
+        String configRootPath = PathUtils.getExternalAppDataPath();//APP外部沙箱根路径(配置文件的更改方式,如果是"手动"修改,则配置文件放在外部沙箱(方便)).
+        String configJson = FileIOUtils.readFile2String(new File(configRootPath, "config"));//读取配置文件.//写入用FileIOUtils.writeFileFromString();
+        if (!TextUtils.isEmpty(configJson)) {
+            try {
+                JSONObject config = new JSONObject(configJson);
+                configJson = "(format OK) " + configJson;
+                isDebugLog = config.getBoolean("debug");
+            } catch (JSONException e) {
+                configJson = "(format NO) " + configJson;
+//                e.printStackTrace();
+            }
+        }
         //通用配置
         LogUtils.getConfig().setGlobalTag("WEI");//TAG不会做落地保存,是记录在运行内存中的.
 //        LogUtils.getConfig().setFilePrefix("log");//设置 log 文件前缀.
         LogUtils.getConfig().setLog2FileSwitch(true);//默认开启日志落地.
-        LogUtils.getConfig().setFileFilter(AppUtils.isAppDebug() ? LogUtils.V : LogUtils.I);//控制日志落地等级.
+        LogUtils.getConfig().setFileFilter(isDebugLog ? LogUtils.V : LogUtils.I);//控制日志落地等级.
         LogUtils.getConfig().setSaveDays(7);//设置log可保留天数.
-        LogUtils.getConfig().setLogHeadSwitch(AppUtils.isAppDebug());//是否展示日志中可跳转到代码的头部信息.
-        LogUtils.getConfig().setBorderSwitch(AppUtils.isAppDebug());//是否展示每条日志的边框图形.
-        LogUtils.getConfig().setConsoleFilter(AppUtils.isAppDebug() ? LogUtils.V : LogUtils.I);//设置AS控制台过滤器.
+        LogUtils.getConfig().setLogHeadSwitch(isDebugLog);//是否展示日志中可跳转到代码的头部信息.
+        LogUtils.getConfig().setBorderSwitch(isDebugLog);//是否展示每条日志的边框图形.
+        LogUtils.getConfig().setConsoleFilter(isDebugLog ? LogUtils.V : LogUtils.I);//设置AS控制台过滤器.
         //设备信息打印.
         LogUtils.i("====================>>Application initApp<<====================",
-                "debug_or_release: " + (AppUtils.isAppDebug() ? "Debug" : "Release"),//是否Debug.
+                "debug_or_release: " + (AppUtils.isAppDebug() ? "Debug" : "Release"),//APK是否为Debug包.
                 "device_model: " + DeviceUtils.getModel(),//设备型号.
                 "device_id: " + DeviceUtils.getUniqueDeviceId(),//设备ID.
                 "android_version: " + Build.VERSION.RELEASE,//Android系统版本.
                 "android_sdk_version: " + Build.VERSION.SDK_INT,//Android API版本.
-                "screenW:" + ScreenUtils.getScreenWidth() + " screenH:" + ScreenUtils.getScreenHeight() + " appW:" + ScreenUtils.getAppScreenWidth() + " appH:" + ScreenUtils.getAppScreenHeight(),//屏幕宽高和应用宽高.
-                AppUtils.getAppInfo()//APP信息.
+                "screenW: " + ScreenUtils.getScreenWidth() + " | screenH: " + ScreenUtils.getScreenHeight() + " | appW: " + ScreenUtils.getAppScreenWidth() + " | appH: " + ScreenUtils.getAppScreenHeight(),//屏幕宽高和应用宽高.
+                "app_info: " + AppUtils.getAppInfo(),//APP信息.
+                "config: " + configJson//配置文件内容(应为Json格式).
         );
     }
 
