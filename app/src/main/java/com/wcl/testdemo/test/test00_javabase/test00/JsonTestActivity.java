1e3aae7d70e29d5c21590849736e4219fe24c39c
chunle.wei <chunle.wei@timanetworks.com>
2023-12-11 18:35:02 +0800

    修改: 优化缓存测试;新增Json测试.

---
+++ app/src/main/java/com/wcl/testdemo/test/test00_javabase/test00/JsonTestActivity.java
@@ -0,0 +1,194 @@
+package com.wcl.testdemo.test.test00_javabase.test00;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
+
+import com.blankj.utilcode.util.GsonUtils;
+import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.ToastUtils;
+import com.wcl.testdemo.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * @Author WCL
+ * @Date 2023/11/30 17:06
+ * @Version
+ * @Description Json测试界面.
+ */
+public class JsonTestActivity extends AppCompatActivity {
+
+    /**
+     * Comment: 用来输出测试结果的控制台.
+     */
+    @BindView(R.id.tv)
+    TextView mTvConsole;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_json_test);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.tv_0, R.id.tv_1, R.id.tv_2, R.id.tv_3, R.id.tv_4, R.id.tv_5, R.id.tv_6, R.id.tv_7, R.id.tv_8, R.id.tv_9, R.id.tv_10, R.id.tv_11, R.id.tv_12, R.id.tv_13, R.id.tv_14, R.id.tv_15, R.id.tv_16, R.id.tv_17, R.id.tv_18, R.id.tv_19, R.id.tv_20})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_0://Json转[普通对象].
+                //构建Json字符串:
+                String json0 = GsonUtils.toJson(new Person("WCL"));//Json字符串.
+                //Json字符串转对象(方式1):
+                Person person0_1 = GsonUtils.fromJson(json0, Person.class);
+                //Json字符串转对象(方式2):
+                Person person0_2 = GsonUtils.fromJson(json0, GsonUtils.getType(Person.class));
+                //打印.
+                String msg0 = "Json转[普通对象]:\n" + json0 + "\n方式一:\n" + person0_1 + "\n方式二:\n" + person0_2;
+                print(msg0);
+                break;
+            case R.id.tv_1://Json转[单层多泛型对象].
+                //构建Json字符串:
+                String json1 = GsonUtils.toJson(new Result<Integer, String, Person>(29, "WCL", new Person("WCL")));//Json字符串.
+                //Json字符串转对象(方式1):
+                Result<Integer, String, Person> result1_1 = GsonUtils.fromJson(json1, Result.class);
+                //Json字符串转对象(方式2):
+                Result<Integer, String, Person> result1_2 = GsonUtils.fromJson(
+                        json1,
+                        GsonUtils.getType(
+                                Result.class, //参数1:总Type.
+                                Integer.class, //可变参数2:泛型1Type.
+                                String.class, //可变参数3:泛型2Type.
+                                Person.class //可变参数4:泛型3Type.
+                        )
+                );
+                //打印.
+                String msg1 = "Json转[单层多泛型对象]:\n" + json1 + "\n方式一:\n" + result1_1 + "\n方式二:\n" + result1_2;
+                print(msg1);
+                break;
+            case R.id.tv_2://Json转[多层多泛型对象].
+                //List:
+                List<Person> personList = new ArrayList<>();
+                personList.add(new Person("List1"));
+                personList.add(new Person("List2"));
+                //Map:
+                Map<String, Person> personMap = new HashMap<>();
+                personMap.put("key1", new Person("Map1"));
+                personMap.put("key2", new Person("Map2"));
+                //数组:
+                Person[] personArr = new Person[]{new Person("Array1"), new Person("Array2")};
+                //构建Json字符串:
+                String json2 = GsonUtils.toJson(new Result<List<Person>, Map<String, Person>, Person[]>(personList, personMap, personArr));//Json字符串.
+                //Json字符串转对象(方式1):
+                Result<List<Person>, Map<String, Person>, Person[]> result2_1 = GsonUtils.fromJson(json2, Result.class);
+                //Json字符串转对象(方式2):
+                Result<Integer, String, Person> result2_2 = GsonUtils.fromJson(
+                        json2,
+                        GsonUtils.getType(
+                                Result.class, //参数1:总Type.
+                                GsonUtils.getListType(Person.class), //可变参数2:泛型1Type.
+                                GsonUtils.getMapType(String.class, Person.class), //可变参数3:泛型2Type.
+                                GsonUtils.getArrayType(Person.class) //可变参数4:泛型3Type.
+                        )
+                );
+                //打印.
+                String msg2 = "Json转[多层多泛型对象]:\n" + json2 + "\n方式一:\n" + result2_1 + "\n方式二:\n" + result2_2;
+                print(msg2);
+                break;
+            case R.id.tv_3://
+                break;
+            case R.id.tv_4://
+                break;
+            case R.id.tv_5://
+                break;
+            case R.id.tv_6://
+                break;
+            case R.id.tv_7://
+                break;
+            case R.id.tv_8://
+                break;
+            case R.id.tv_9://
+                break;
+            case R.id.tv_10://
+                break;
+            case R.id.tv_11://
+                break;
+            case R.id.tv_12://
+                break;
+            case R.id.tv_13://
+                break;
+            case R.id.tv_14://
+                break;
+            case R.id.tv_15://
+                break;
+            case R.id.tv_16://
+                break;
+            case R.id.tv_17://
+                break;
+            case R.id.tv_18://
+                break;
+            case R.id.tv_19://
+                break;
+            case R.id.tv_20://
+                break;
+        }
+    }
+
+    //一键三连,在三个地方输出打印结果.
+    private void print(String msg) {
+        if (!TextUtils.isEmpty(msg)) {
+            LogUtils.d(msg);
+            ToastUtils.showShort(msg);
+            mTvConsole.setText(msg);
+        }
+    }
+
+    /**
+     * @Author WCL
+     * @Date 2023/11/30 17:14
+     * @Version
+     * @Description
+     */
+    static class Result<K, T, V> {
+        int code;
+        String message;
+        K kData;
+        T tData;
+        V vData;
+
+        Result(K kData, T tData, V vData) {
+            this.code = 200;
+            this.message = "success";
+            this.kData = kData;
+            this.tData = tData;
+            this.vData = vData;
+        }
+    }
+
+
+    /**
+     * @Author WCL
+     * @Date 2023/11/30 17:14
+     * @Version
+     * @Description
+     */
+    static class Person {
+
+        String name;
+        int age;
+        String address;
+
+        Person(String name) {
+            this.name = name;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/wcl/testdemo/test/test00_javabase/test01/CacheTestActivity.java
@@ -0,0 +1,153 @@
+package com.wcl.testdemo.test.test00_javabase.test01;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
+
+import com.blankj.utilcode.util.CacheDiskStaticUtils;
+import com.blankj.utilcode.util.CacheDiskUtils;
+import com.blankj.utilcode.util.CacheDoubleStaticUtils;
+import com.blankj.utilcode.util.CacheDoubleUtils;
+import com.blankj.utilcode.util.CacheMemoryStaticUtils;
+import com.blankj.utilcode.util.CacheMemoryUtils;
+import com.blankj.utilcode.util.GsonUtils;
+import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.ToastUtils;
+import com.blankj.utilcode.util.Utils;
+import com.wcl.testdemo.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * @Author WCL
+ * @Date 2023/12/11 17:47
+ * @Version
+ * @Description 缓存测试界面
+ */
+public class CacheTestActivity extends AppCompatActivity {
+
+    /**
+     * Comment: 用来输出测试结果的控制台.
+     */
+    @BindView(R.id.tv)
+    TextView mTvConsole;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cache_test);
+        ButterKnife.bind(this);
+        initCacheUtils();//初始化缓存工具.
+    }
+
+    @OnClick({R.id.tv_0, R.id.tv_1, R.id.tv_2, R.id.tv_3, R.id.tv_4, R.id.tv_5, R.id.tv_6, R.id.tv_7, R.id.tv_8, R.id.tv_9, R.id.tv_10, R.id.tv_11, R.id.tv_12, R.id.tv_13, R.id.tv_14, R.id.tv_15, R.id.tv_16, R.id.tv_17, R.id.tv_18, R.id.tv_19, R.id.tv_20})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_0://二级缓存(将对象,缓存到内存及本地).
+                saveObject();
+                break;
+            case R.id.tv_1://二级缓存(将对象,从缓存中取出).
+                getObject();
+                break;
+            case R.id.tv_2://
+                break;
+            case R.id.tv_3://
+                break;
+            case R.id.tv_4://
+                break;
+            case R.id.tv_5://
+                break;
+            case R.id.tv_6://
+                break;
+            case R.id.tv_7://
+                break;
+            case R.id.tv_8://
+                break;
+            case R.id.tv_9://
+                break;
+            case R.id.tv_10://
+                break;
+            case R.id.tv_11://
+                break;
+            case R.id.tv_12://
+                break;
+            case R.id.tv_13://
+                break;
+            case R.id.tv_14://
+                break;
+            case R.id.tv_15://
+                break;
+            case R.id.tv_16://
+                break;
+            case R.id.tv_17://
+                break;
+            case R.id.tv_18://
+                break;
+            case R.id.tv_19://
+                break;
+            case R.id.tv_20://
+                break;
+        }
+    }
+
+    //自定义初始化缓存工具(所有初始化都可省略,省略时即使用默认).
+    private void initCacheUtils() {
+        //(1) TODO: 2023/12/11 内存缓存相关: [内存缓存工具:CacheMemoryUtils]和[静态内存缓存工具:CacheMemoryStaticUtils].
+        //内存缓存工具:
+        CacheMemoryUtils memoryUtils = CacheMemoryUtils.getInstance();//[内存缓存工具].
+        //静态内存缓存工具:
+        CacheMemoryStaticUtils.setDefaultCacheMemoryUtils(memoryUtils);//为[静态内存缓存工具]设置默认的[内存缓存工具],之后即可通过[静态内存缓存工具]的静态方法操作默认的[内存缓存工具]对应的非静态方法了.
+
+        //(2) TODO: 2023/12/11 磁盘缓存相关: [磁盘缓存工具:CacheDiskUtils]和[静态磁盘缓存工具:CacheDiskStaticUtils].
+        //磁盘缓存工具:
+        CacheDiskUtils diskUtils = CacheDiskUtils.getInstance(Utils.getApp().getExternalCacheDir());//无参情况下默认缓存到内部沙箱的缓存目录.
+        //静态磁盘缓存工具:
+        CacheDiskStaticUtils.setDefaultCacheDiskUtils(diskUtils);//为[静态磁盘缓存工具]设置默认的[磁盘缓存工具],之后即可通过[静态磁盘缓存工具]的静态方法操作默认的[磁盘缓存工具]对应的非静态方法了.
+
+        //(3) TODO: 2023/12/11 二级缓存相关: [二级缓存工具:CacheDoubleUtils]和[静态二级缓存工具:CacheDoubleStaticUtils].
+        /*
+        CacheDoubleUtils.java(二级缓存工具)中包含了CacheMemoryUtils.java(内存缓存工具)和CacheDiskUtils.java(磁盘缓存工具).
+        通过"二级缓存工具"存对象时,会在"磁盘缓存工具"和"内存缓存工具"各存一份.
+        通过"二级缓存工具"取对象时,会先在"内存缓存工具"中找,找到就返回该对象.找不到再在"磁盘缓存工具"中找,找到会先拷贝一份到"内存缓存工具"中再返回该对象,找不到就返回之前设置的默认值或null.
+        */
+        //二级缓存工具:
+        CacheDoubleUtils doubleUtils = CacheDoubleUtils.getInstance(memoryUtils, diskUtils);//[二级缓存工具]:
+        //静态二级缓存工具:
+        CacheDoubleStaticUtils.setDefaultCacheDoubleUtils(doubleUtils);//为[静态二级缓存工具]设置默认的[二级缓存工具],之后即可通过[静态二级缓存工具]的静态方法操作默认的[二级缓存工具]对应的非静态方法了.
+    }
+
+    //二级缓存(将对象,缓存到内存及本地).
+    private void saveObject() {
+        //缓存数据(可存储类型:byte[],String,JSONObject,JSONArray,Bitmap,Drawable,Parcelable,Serializable).
+        CacheDoubleStaticUtils.put("ARR", new byte[]{1, 2, 3}); //存字节数组.
+        CacheDoubleStaticUtils.put("APP_NAME", getString(R.string.app_name)); //存String.
+        print("二级缓存成功,可去下面路径查看:\n" + Utils.getApp().getExternalCacheDir().getAbsolutePath());
+    }
+
+    //二级缓存(将对象,从缓存中取出).
+    private void getObject() {
+        String appName = CacheDoubleStaticUtils.getString("APP_NAME");
+        byte[] arr = CacheDoubleStaticUtils.getBytes("ARR");
+        StringBuilder sb = new StringBuilder()
+                .append("获取本地存储的二级缓存.\n")
+                .append("\nAPP_NAME: ")
+                .append(appName)
+                .append("\nARR: ")
+                .append(GsonUtils.toJson(arr));
+        print(sb.toString());
+    }
+
+    //一键三连,在三个地方输出打印结果.
+    private void print(String msg) {
+        if (!TextUtils.isEmpty(msg)) {
+            LogUtils.d(msg);
+            ToastUtils.showShort(msg);
+            mTvConsole.setText(msg);
+        }
+    }
+
+}
