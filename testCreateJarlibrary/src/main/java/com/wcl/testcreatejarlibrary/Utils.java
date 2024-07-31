5f66fb77721b0ede31786d40661067d97864e283
chunle.wei <chunle.wei@timanetworks.com>
2024-07-31 18:29:40 +0800

    新增: 测试将java代码打包成jar.

---
+++ testCreateJarlibrary/src/main/java/com/wcl/testcreatejarlibrary/Utils.java
@@ -0,0 +1,15 @@
+package com.wcl.testcreatejarlibrary;
+
+/**
+ * @Author WCL
+ * @Date 2024/7/31 18:13
+ * @Version
+ * @Description 测试将java工具代码打包成jar.
+ */
+public class Utils {
+
+    public static String getLog() {
+        return "jar-->返回的数据";
+    }
+
+}
